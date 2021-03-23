075debbaf19852d7a2f5b88721a4059098b05e9e
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2021-03-23 13:02:50 +1000

    Add files via upload

---
+++ jsPsych/AMP.js
@@ -0,0 +1,42 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'Physical Activity',  //Will be used in the user feedback 
+				nameForLogging : 'Physical Activity', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'exercise'}, 
+					{word : 'workout'}, 
+					{word : 'running'}, 
+					{word : 'active'}]
+
+			}, 
+			{
+				nameForFeedback : 'Sedentary Behaviour',  //Will be used in the user feedback 
+				nameForLogging : 'Sedentary Behaviour', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'resting'}, 
+					{word : 'sitting'}, 
+					{word : 'lying down'}, 
+					{word : 'couch'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		}
+	});
+});
