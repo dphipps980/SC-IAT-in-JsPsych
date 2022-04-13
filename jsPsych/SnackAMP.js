5a9207ad1436c0692163cfdaa75b07ebfb4dbce0
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2022-04-13 11:55:16 +1000

    Add files via upload

---
+++ jsPsych/SnackAMP.js
@@ -0,0 +1,44 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'Physical Activity',  //Will be used in the user feedback 
+				nameForLogging : 'Physical Activity', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'Chocolate'}, 
+					{word : 'Chips'}, 
+					{word : 'Biscuits'}, 
+                    {word : 'Lollies'}, 
+					{word : 'Cake'}]
+
+			}, 
+			{
+				nameForFeedback : 'Sedentary Behaviour',  //Will be used in the user feedback 
+				nameForLogging : 'Sedentary Behaviour', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'Banana'}, 
+					{word : 'Apple'}, 
+					{word : 'Strawberry'}, 
+                    {word : 'Mango'}, 
+					{word : 'Oranges'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		}
+	});
+});
