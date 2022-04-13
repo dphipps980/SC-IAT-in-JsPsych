2e5f8b1b9db04a88efb0ae66e71f461adf056d89
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2022-04-13 13:13:55 +1000

    Update AlcAMP.js

--- jsPsych/AlcAMP.js
+++ jsPsych/AlcAMP.js
@@ -37,8 +37,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
 		},
 		base_url : {//Where are your images at?
-		image : 'https://dphipps980.github.io/SC-IAT-in-JsPsych/jsPsych/Images/',
-		image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		image : 'https://dphipps980.github.io/SC-IAT-in-JsPsych/jsPsych/Images/'
 		}
 	});
 });
