ac0c76cb302166c153c1523f2dd4b4e919b77979
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2024-03-04 16:12:09 +0200

    Create SprtSclIAT.js

---
+++ jsPsych/SprtSclIAT.js
@@ -0,0 +1,41 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Sport', //Will appear in the data.
+			title : {
+				media : {word : 'Sport'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'school'}, 
+    			{image : 'class'}, 
+    			{image : 'learning'}, 
+    			{image : 'class'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'School', //Will appear in the data.
+			title : {
+				media : {word : 'School'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'sport'}, 
+    			{image : 'training'}, 
+    			{image : 'exercise'}, 
+    			{image : 'coaching'}], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
+});
