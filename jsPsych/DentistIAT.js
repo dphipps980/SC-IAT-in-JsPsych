3ca2544690fe57dbc043992446226077aa9ddd2c
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2024-03-21 13:13:17 +0200

    Add files via upload

---
+++ jsPsych/DentistIAT.js
@@ -0,0 +1,69 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+	
+	var API = new APIConstructor();
+		  return stiatExtension({
+		  category : { 
+		    name : 'Dental Check-ups', //Will appear in the data.
+		    title : {
+		      media : {word : 'Dental Check-ups'}, //Name of the category presented in the task.
+		      css : {color:'#000000','font-size':'2em'}, //Style of the category title.
+		      height : 7 //Used to position the "Or" in the combined block.
+		    }, 
+		    media : [ //Stimuli content as PIP's media objects
+    		    	{image : 'P1.png'}, 
+    			{image : 'P2.png'}, 
+    			{image : 'P3.png'}, 
+    			{image : 'P4.png'}, 
+    			{image : 'P5.png'}, 
+    			{image : 'P6.png'},
+				{image : 'P7.png'},
+				{image : 'P8.png'}
+		    ], 
+		    //Stimulus css (style)
+		    css : {color:'#31b404','font-size':'3em'}
+		  },	
+
+  		attribute1 : 
+			{
+			name : 'Unpleasant', //Attribute label
+			title : {
+				media : {word : 'Negative'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Bomb'},
+				{word: 'Abuse'},
+				{word: 'Sadness'},
+				{word: 'Pain'},
+				{word: 'Poison'},
+				{word: 'Grief'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+		attribute2 : 
+			{
+			name : 'Pleasant', //Attribute label
+			title : {
+				media : {word : 'Positive'}, //Name of the category presented in the task.
+				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+				height : 7 //Used to position the "Or" in the combined block.
+			}, 
+			media : [ //Stimuli
+				{word: 'Paradise'},
+				{word: 'Pleasure'},
+				{word: 'Cheer'},
+				{word: 'Wonderful'},
+				{word: 'Splendid'},
+				{word: 'Love'}
+			], 
+			//Can change color and size of the targets here.
+			css : {color:'#31b404','font-size':'3em'}
+			},
+
+  base_url : {//Where are your images at?
+    image : 'https://dphipps980.github.io/SC-IAT-in-JsPsych/jsPsych/Images/'
+  }}
+  );
+  });
