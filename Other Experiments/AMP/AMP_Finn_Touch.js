a9e8312834f69299cd2f7219abf907c700733a23
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2025-04-17 09:44:02 +0300

    Add files via upload

---
+++ Other Experiments/AMP/AMP_Finn_Touch.js
@@ -0,0 +1,44 @@
+define(['pipAPI', 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experiments/AMPs/qamp_Finn_Touch.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'Physical Activity',  //Will be used in the user feedback 
+				nameForLogging : 'Physical Activity', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'Liikunta'}, 
+					{word : 'juoksu'}, 
+					{word : 'Treeni'}, 
+					{word : 'Fyysinen aktiivisuus'}, 
+					{word : 'Liikkuminen'}]
+
+			}, 
+			{
+				nameForFeedback : 'Sedentary Behaviour',  //Will be used in the user feedback 
+				nameForLogging : 'Sedentary Behaviour', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+					{word : 'Lepääminen'}, 
+					{word : 'Istuminen'}, 
+					{word : 'Tuoli'},
+					{word : 'Sohva'},
+					{word : 'makaaminen'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		}
+	});
+});
