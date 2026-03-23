d777c65b87448ddccd5324e2321aba5c91610889
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2026-03-23 13:04:48 +0200

    Update AMP_Nic_Finn.js

--- Other Experiments/AMP/AMP_Nic_Finn.js
+++ Other Experiments/AMP/AMP_Nic_Finn.js
@@ -2,48 +2,46 @@ define(['pipAPI', 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experi
 
 	var API = new APIConstructor();
 
-	var baseStimuli = 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experiments/AMP/Stimuli/';
-
 	return ampExtension({
 		primeCats : [
 			{
 				nameForFeedback : 'Vaping',
 				nameForLogging : 'vaping',
 				mediaArray : [
-					{image : baseStimuli + 'Vape1.png'},
-					{image : baseStimuli + 'Vape2.png'},
-					{image : baseStimuli + 'Vape3.png'},
-					{image : baseStimuli + 'Vape4.png'},
-					{image : baseStimuli + 'Vape5.png'}
+					{image : 'Vape1.png'},
+					{image : 'Vape2.png'},
+					{image : 'Vape3.png'},
+					{image : 'Vape4.png'},
+					{image : 'Vape5.png'}
 				]
 			},
 			{
 				nameForFeedback : 'Nicotine Pouches',
 				nameForLogging : 'pouches',
 				mediaArray : [
-					{image : baseStimuli + 'Pouch1.png'},
-					{image : baseStimuli + 'Pouch2.png'},
-					{image : baseStimuli + 'Pouch3.png'},
-					{image : baseStimuli + 'Pouch4.png'},
-					{image : baseStimuli + 'Pouch5.png'}
+					{image : 'Pouch1.png'},
+					{image : 'Pouch2.png'},
+					{image : 'Pouch3.png'},
+					{image : 'Pouch4.png'},
+					{image : 'Pouch5.png'}
 				]
 			},
 			{
 				nameForFeedback : 'Cigarettes',
 				nameForLogging : 'cigarettes',
 				mediaArray : [
-					{image : baseStimuli + 'Cig1.png'},
-					{image : baseStimuli + 'Cig2.png'},
-					{image : baseStimuli + 'Cig3.png'},
-					{image : baseStimuli + 'Cig4.png'},
-					{image : baseStimuli + 'Cig5.png'}
+					{image : 'Cig1.png'},
+					{image : 'Cig2.png'},
+					{image : 'Cig3.png'},
+					{image : 'Cig4.png'},
+					{image : 'Cig5.png'}
 				]
 			},
 			{
 				nameForFeedback : 'Neutral',
 				nameForLogging : 'neutral',
 				mediaArray : [
-					{image : baseStimuli + 'Neutral1.png'}
+					{image : 'Neutral1.png'}
 				]
 			}
 		],
@@ -54,7 +52,7 @@ define(['pipAPI', 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experi
 		},
 
 		base_url : {
-			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+			image : 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experiments/AMP/Stimuli/'
 		}
 	});
 });
