609bcd7b47112683619e83c9a4a1d6497e822e12
dphipps980 <71249207+dphipps980@users.noreply.github.com>
2026-03-23 13:13:56 +0200

    Create AMP_Nic_Finn_Touch.js

---
+++ Other Experiments/AMP/AMP_Nic_Finn_Touch.js
@@ -0,0 +1,58 @@
+define(['pipAPI', 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experiments/AMP/qamp_Finn_Touch.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+
+	return ampExtension({
+		primeCats : [
+			{
+				nameForFeedback : 'Vaping',
+				nameForLogging : 'vaping',
+				mediaArray : [
+					{image : 'Vape1.png'},
+					{image : 'Vape2.png'},
+					{image : 'Vape3.png'},
+					{image : 'Vape4.png'},
+					{image : 'Vape5.png'}
+				]
+			},
+			{
+				nameForFeedback : 'Nicotine Pouches',
+				nameForLogging : 'pouches',
+				mediaArray : [
+					{image : 'Pouch1.png'},
+					{image : 'Pouch2.png'},
+					{image : 'Pouch3.png'},
+					{image : 'Pouch4.png'},
+					{image : 'Pouch5.png'}
+				]
+			},
+			{
+				nameForFeedback : 'Cigarettes',
+				nameForLogging : 'cigarettes',
+				mediaArray : [
+					{image : 'Cig1.png'},
+					{image : 'Cig2.png'},
+					{image : 'Cig3.png'},
+					{image : 'Cig4.png'},
+					{image : 'Cig5.png'}
+				]
+			},
+			{
+				nameForFeedback : 'Neutral',
+				nameForLogging : 'neutral',
+				mediaArray : [
+					{image : 'Neutral1.png'}
+				]
+			}
+		],
+
+		examplePrimeStimulus : {
+			nameForLogging : 'examplePrime',
+			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
+		},
+
+		base_url : {
+			image : 'https://dphipps980.github.io/SC-IAT-in-JsPsych/Other%20Experiments/AMP/Stimuli/'
+		}
+	});
+});
